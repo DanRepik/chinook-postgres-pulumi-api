0ced1f12b81788ef76f68385e9f5b1ff36558cde
Dan Repik <danrepik@gmail.com>
2024-09-10 08:59:13 -0400

    Work on tests

--- tests/test_albums.py
+++ tests/test_albums.py
@@ -30,9 +30,30 @@ def test_get_request_all(gateway_endpoint):
         'title': 'For Those About To Rock We Salute You'
     }
 
-def test_get_request_one(gateway_endpoint):
+def test_get_request_query_one(gateway_endpoint):
     # Define the endpoint
     endpoint = gateway_endpoint + "/album?album_id=5"
+
+    log.info(f"request: {endpoint}")
+
+    # Send the GET request
+    start = time.perf_counter()
+    response = requests.get(endpoint)
+    log.info(f"response time: {time.perf_counter()-start}")
+
+    # Validate the response status code
+    assert (
+        response.status_code == 200
+    ), f"Expected status code 200, got {response.status_code}"
+
+    # Validate the response content
+    albums = response.json()
+    assert len(albums) == 1
+    assert albums[0] == {'album_id': 5, 'artist_id': 3, 'title': 'Big Ones'}
+    
+def test_get_request_by_id(gateway_endpoint):
+    # Define the endpoint
+    endpoint = gateway_endpoint + "/album/5"
 
     log.info(f"request: {endpoint}")
 
