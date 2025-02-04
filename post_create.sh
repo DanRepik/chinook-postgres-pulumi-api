43b53f4ebcb2b8f62c2f48601bd848b64e13c9a5
Dan Repik <danrepik@gmail.com>
2025-02-04 14:07:08 +0000

    Work on codespaces

--- post_create.sh
+++ post_create.sh
@@ -2,6 +2,5 @@ python -m venv venv
 source venv/bin/activate
 sudo pip install -r requirements.txt
 playground postgres
-export PULUMI_CONFIG_PASSPHRASE=Xxxxx-Xxxxx
 pulumi login -l
 pulumi stack init local
